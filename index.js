dc69ee110e31de6a861c21d723611267771bd1f3
ashikur <ashikur1602@gmail.com>
2024-06-13 18:47:19 +0600

    seminar admission blog api created

---
+++ index.js
@@ -0,0 +1,116 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+
+// middleware
+
+app.use(cors());
+app.use(express.json());
+
+
+const { MongoClient, ServerApiVersion,  ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bpilnp1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+      await client.connect();
+      const admissionCollection = client.db('BIFDT').collection('admission');
+      const seminarCollection = client.db('BIFDT').collection('seminar');
+      const blogCollection = client.db('BIFDT').collection('blog');
+
+
+      app.get('/admissionList', async (req, res) => {
+        const cursor = admissionCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+
+      //   admission api 
+      
+      app.post('/admission', async (req, res) => {
+          const admissionRequest = req.body;
+          const result = await admissionCollection.insertOne(admissionRequest);
+          res.send(result);
+      })
+
+      app.get('/admission', async (req, res) => {
+          const cursor = admissionCollection.find();
+          const result = await cursor.toArray();
+          res.send(result);
+      })
+
+      app.delete('/admission/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) }
+        const result = await admissionCollection.deleteOne(query);
+        res.send(result);
+      })
+      
+      //   seminar api
+      
+      app.post('/seminar', async (req, res) => {
+          const seminarRequest = req.body;
+          const result = await seminarCollection.insertOne(seminarRequest);
+          res.send(result);
+      })
+
+      app.get('/seminar', async (req, res) => {
+          const result = await seminarCollection.find().toArray();
+          res.send(result);
+      })
+
+      app.delete('/seminar/:id', async (req, res) => {
+          const id = req.params.id;
+          const query = { _id: new ObjectId(id) }
+          const result = await seminarCollection.deleteOne(query);
+          res.send(result);
+      })
+
+      //   blog api
+      
+      app.post('/blog', async (req, res) => {
+          const blog = req.body;
+          const result = await blogCollection.insertOne(blog);
+          res.send(result);
+      })
+
+
+      app.get('/blog', async (req, res) => {
+          const result = await blogCollection.find().toArray();
+          res.send(result);
+      })
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+    res.send('server is ok');
+})
+
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`)
+})
