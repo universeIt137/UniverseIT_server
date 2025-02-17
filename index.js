dd00a9b3e82177f79a8dc4b86c5325388f6cec9f
ishanrana363 <ishanrana363@gmail.com>
2025-02-17 17:50:14 +0600

    addded

--- index.js
+++ index.js
@@ -1342,11 +1342,15 @@ async function run() {
     // video related api
 
 
+    // vidoe upload api
+
     app.post("/video-upload", async (req, res) => {
       const reqBody = req.body;
       const data = await videoCollection.insertOne(reqBody);
       res.send(data);
-    });
+    });  
+
+    // all video find api
 
     app.get("/all-video", async (req, res) => {
       const data = await videoCollection.find().toArray();
